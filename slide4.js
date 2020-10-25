374c1756775c2164f05ee465b7980a7fd93eaa56
Adi Eyal <adi@openup.org.za>
2020-10-25 20:29:05 +0200

    Added slide4

--- slide4.js
+++ slide4.js
@@ -4,11 +4,31 @@ import BaseSlide from './base_slide';
 // source: competition tribunal
 const clicksSales = {data: [
     {topicName: 'Clicks mask sales', name: 1, date: '2019-11-01', value: 0.834},
-    {topicName: 'Clicks mask sales', name: 2, date: '2019-12-01', value: 0.834},
-    {topicName: 'Clicks mask sales', name: 3, date: '2020-01-01', value: 0.9556},
-    {topicName: 'Clicks mask sales', name: 4, date: '2020-02-01', value: 1.5644},
-    {topicName: 'Clicks mask sales', name: 4, date: '2020-03-01', value: 3.4774},
-    {topicName: 'Clicks mask sales2', name: 4, date: '2020-03-01', value: 8.4774},
+    {topicName: 'Clicks mask sales', name: 1, date: '2019-12-01', value: 0.834},
+    {topicName: 'Clicks mask sales', name: 1, date: '2020-01-01', value: 0.9556},
+    {topicName: 'Clicks mask sales', name: 1, date: '2020-02-01', value: 1.5644},
+    {topicName: 'Clicks mask sales', name: 1, date: '2020-03-01', value: 3.4774},
+    {topicName: 'Rand Safety Equipment CC', name: 2, date: '2019-12-01', value: 5.75},
+    {topicName: 'Rand Safety Equipment CC', name: 2, date: '2020-01-01', value: 5.75},
+    {topicName: 'Rand Safety Equipment CC', name: 2, date: '2020-02-01', value: 7.65},
+    {topicName: 'Rand Safety Equipment CC', name: 2, date: '2020-03-01', value: 7.65},
+    {topicName: 'Rand Safety Equipment CC', name: 2, date: '2020-04-01', value: 7.65},
+    {topicName: 'Levtrade International (Pty) Ltd', name: 3, date: '2020-02-01', value: 7.9925},
+    {topicName: 'Levtrade International (Pty) Ltd', name: 3, date: '2020-03-01', value: 17.825},
+    {topicName: 'Steelmate (Pty) Ltd', name: 4, date: '2020-04-01', value: 31.6},
+    {topicName: 'Steelmate (Pty) Ltd', name: 5, date: '2020-05-01', value: 31.45},
+    {topicName: 'Steelmate (Pty) Ltd', name: 5, date: '2020-05-01', value: 31.45},
+    {topicName: 'Vasilis Cleaning Supplies', name: 6, date: '2019-12-01', value: 2.8},
+    {topicName: 'Vasilis Cleaning Supplies', name: 6, date: '2020-02-01', value: 5.652},
+    {topicName: 'Vasilis Cleaning Supplies', name: 6, date: '2020-03-01', value: 21.25},
+    {topicName: 'Basic Trading', name: 7, date: '2020-01-01', value: 3.6},
+    {topicName: 'Basic Trading', name: 7, date: '2020-02-01', value: 24.99},
+    {topicName: 'Basic Trading', name: 7, date: '2020-04-01', value: 26.5},
+    {topicName: 'Babelegi Workwear', name: 8, date: '2019-12-01', value: 2.53},
+    {topicName: 'Babelegi Workwear', name: 8, date: '2020-01-01', value: 4.55},
+    {topicName: 'Babelegi Workwear', name: 8, date: '2020-01-01', value: 4.55},
+    {topicName: 'Babelegi Workwear', name: 8, date: '2020-02-01', value: 17.25},
+    {topicName: 'Babelegi Workwear', name: 8, date: '2020-03-01', value: 27.5}
 ]}
 
 
